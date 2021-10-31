0ec3c04f3f860d0c4df8d5cdceef7b8ea2002ef5
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2021-10-31 10:10:06 -0300

    init homologação
    Foi adicionado as telas de tarefas e usuarios, o cadastro de usuarios ja esta salvando no banco, fica para teste a listagem de usuarios vinda do banco

---
+++ precache-manifest.b29e452788eb61f986e35beb7a5ebf50.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1e7c4b33f6a509483b",
+    "url": "/many-tarefas/css/app.cbc8dfe3.css"
+  },
+  {
+    "revision": "9c598c5b20962f0e6d63",
+    "url": "/many-tarefas/css/chunk-186c6e98.f021be6d.css"
+  },
+  {
+    "revision": "64d5bfa2f3ea0c1bcca3",
+    "url": "/many-tarefas/css/chunk-533d7720.8971f386.css"
+  },
+  {
+    "revision": "718643b78f35cbca10d7",
+    "url": "/many-tarefas/css/chunk-5433911f.f3ef123a.css"
+  },
+  {
+    "revision": "a4343925daa33988779e",
+    "url": "/many-tarefas/css/chunk-704a0174.bcc475c4.css"
+  },
+  {
+    "revision": "bcf4b7f5f7271d687dc21be683d1b861",
+    "url": "/many-tarefas/index.html"
+  },
+  {
+    "revision": "ba1e7c4b33f6a509483b",
+    "url": "/many-tarefas/js/app.6f67fab8.js"
+  },
+  {
+    "revision": "9c598c5b20962f0e6d63",
+    "url": "/many-tarefas/js/chunk-186c6e98.a6196938.js"
+  },
+  {
+    "revision": "e16d4856cfde8c4a01f5",
+    "url": "/many-tarefas/js/chunk-2d21a3d2.97ba7865.js"
+  },
+  {
+    "revision": "cbcf6687ee8b17cdab25",
+    "url": "/many-tarefas/js/chunk-2d22d746.6bfd0836.js"
+  },
+  {
+    "revision": "0edb953f588c87716530",
+    "url": "/many-tarefas/js/chunk-46ea1636.7158be3c.js"
+  },
+  {
+    "revision": "64d5bfa2f3ea0c1bcca3",
+    "url": "/many-tarefas/js/chunk-533d7720.6120bf61.js"
+  },
+  {
+    "revision": "718643b78f35cbca10d7",
+    "url": "/many-tarefas/js/chunk-5433911f.9d925e48.js"
+  },
+  {
+    "revision": "a4343925daa33988779e",
+    "url": "/many-tarefas/js/chunk-704a0174.885bd902.js"
+  },
+  {
+    "revision": "c701ac5ab9e852ad78c3",
+    "url": "/many-tarefas/js/chunk-f3e8821c.a1f5c3ca.js"
+  },
+  {
+    "revision": "4fc2ae76cf71e15bfeec",
+    "url": "/many-tarefas/js/chunk-vendors.3166afa8.js"
+  },
+  {
+    "revision": "2a8aa7b08861f47ed5c3ece6d1dd398c",
+    "url": "/many-tarefas/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/many-tarefas/robots.txt"
+  }
+]);
